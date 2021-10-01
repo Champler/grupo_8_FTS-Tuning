e4f05e925a3583b87229f8dc6eeb544119d0309a
fcicerelli <fcicerellitic@gmail.com>
2021-10-01 08:36:57 -0300

    cotrolador de detalle de producto

--- FTS-Tunning/src/controllers/productosController.js
+++ FTS-Tunning/src/controllers/productosController.js
@@ -1,4 +1,4 @@
-const { products } = require('../data/productsDB');
+//const { products } = require('../data/productsDB');
 const db = require('../database/models');
 
 
@@ -11,23 +11,35 @@ module.exports = {
         .catch(err => console.log(err))
     },
     detalle: (req, res) => {                             //  Fabio
-        let paramsIdProducto = req.params.idProducto.trim();
-        let producto = products.find(element => element.id === +paramsIdProducto)
-        if (producto !== undefined){
-            /* let sliderProducts = products.filter(item => item.category === producto.category) */
-            let sliderProducts = products;    //  <----   Sololamente hay 2 productos por categoría, así que el slider muestra los 12 productos que hay hasta el momento
-            let texto = producto.description.split('\r\n') 
-            res.render('ProductoDetalle', {
-                sliderTitle : "También te pueden interesar",
-                sliderProducts,
-                producto,
-                texto,
-                title: producto.name + " | FTS-Tuning",
-                session: req.session ? req.session : ""
-            })
-        }else{
-            res.send('No tenemos ese producto, pero tenemos muchos otros más...')
-        }
+        const producto = db.Product.findByPk(req.params.idProducto, {
+            include: [{association: "category"}, {association: "images"}]
+        })
+        /* const slider = db.Product.findAll({
+            include: [{association: "category"}, {association: "images"}]
+        })
+        Promise.all([producto,slider])
+        .then((producto, slider) => { */
+        .then((producto) => { 
+        
+            if (producto !== undefined){ 
+                //let sliderProducts = slider;    //  <----   Sololamente hay 2 productos por categoría, así que el slider muestra los 12 productos que hay hasta el momento
+                let texto = producto.description.split('\r\n')
+                console.log(">>>  " + texto + "   <<<")
+                res.render('ProductoDetalle', {
+                    sliderTitle : "También te pueden interesar",
+                    //sliderProducts,
+                    producto,
+                    texto,
+                    n: false,
+                    pk: req.params.idProducto,
+                    title: producto.name + " | FTS-Tuning",
+                    session: req.session ? req.session : ""
+                }) 
+            }else{
+                res.send('No tenemos ese producto, pero tenemos muchos otros más...')
+            }
+        })
+        .catch(error => console.log(error))
     },
     carrito: (req, res) =>{
         res.render('Carrito', {title: "Carrito", session: req.session ? req.session : ""})
@@ -43,16 +55,21 @@ module.exports = {
         })
         Promise.all([category, products])
         .then(([category, products] )=> {
+            
             let filtradosPorCategoria = []
             products.forEach(producto =>{
                 if(producto.category_id === category.id){
                     filtradosPorCategoria.push(producto)
                 }
             })
+            
+            let n;
             if(filtradosPorCategoria.length > 0){
+                n: true;
                 res.render('Productos', {products: filtradosPorCategoria,n,category,title:"Categoría", session: req.session ? req.session : ""})
             }else {
-                res.render('Productos', {products, title:"Categoria",category,session: req.session ? req.session : ""})
+                n: false;
+                res.render('Productos', {products, title:"Categoria",n,category,session: req.session ? req.session : ""})
             }   
         })
     }
